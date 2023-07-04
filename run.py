0b5055d3bd769cce454e7a832ad5c62cc12c1b7e
dussy42 <98059288+dussy42@users.noreply.github.com>
2023-07-04 15:18:36 +0100

    validating that the numbers entered were interger of upto 6 in lenght

--- run.py
+++ run.py
@@ -1,3 +1,44 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+
+sales = SHEET.worksheet('sales')
+
+data = sales.get_all_values()
+
+print(data)
+
+def get_sales():
+    """
+    Get sales figures input from the user
+    """
+    data_str=input("enter your data here:")
+   
+    sales_data=data_str.split(",")
+    validate_data(sales_data)
+
+
+def validate_data(values):
+    """
+    converts all string values into integers. 
+    Raises ValueError if strings cannot be converted
+    into int, or if there aren't exactly 6values.
+    """
+    try:
+        if len(values) !=6:
+            raise ValueError(
+                f'Exactly 6 value required, you provided {len(values)}'
+            )
+    except ValueError as e:
+        print(f'invalid data:{e}, please try again. \n')
+get_sales()
